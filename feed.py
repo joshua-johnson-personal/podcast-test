725baaf1baa59690e1569a66629067487f96272c
joshua-johnson-personal <164550893+joshua-johnson-personal@users.noreply.github.com>
2024-04-03 17:46:55 +0000

    committed

--- feed.py
+++ feed.py
@@ -6,7 +6,7 @@ with open('feed.yaml', 'r') as file:
 
     rss_element = xml_tree.Element('rss', {'version':'2.0',
         'xmlns:itunes':'http://www.itunes.com/dtds/podcast-1.0.dtd',
-        'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
+        'xmlns:content':'https://web.resource.org/rss/1.0/modules/content/'})
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
